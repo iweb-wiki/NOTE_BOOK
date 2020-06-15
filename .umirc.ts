aec19db2aa13017e62e573c4bab300b97b384f89
wang ps <wangps@10.0.0.7>
2020-06-16 02:15:53 +0800

    feat: umirc

--- .umirc.ts
+++ .umirc.ts
@@ -8,5 +8,9 @@ export default defineConfig({
   logo:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  ssr: {},
+  base: '/note',
+  publicPath: '/note/',
+  exportStatic: {},
   // more config: https://d.umijs.org/config
 });
